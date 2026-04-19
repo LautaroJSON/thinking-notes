219b564814096351c4f3d98122a7ea42603ed5fa
Lautaro Fernandez <fernandez.n.lautaro@gmail.com>
2026-04-19 23:45:17 +0000

    fix: env

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,6 +1,6 @@
 import { supabase } from "../../lib/supabaseClient";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL_LOCAL as string;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 
 if (!API_BASE_URL) {
   throw new Error("VITE_API_BASE_URL no está definida en el entorno.");
